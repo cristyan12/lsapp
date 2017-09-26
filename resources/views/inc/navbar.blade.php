8ae7825fecd1af22aa58fa4beef78a3fc32789f2
= <numenor21@gmail.com>
2017-09-25 20:23:43 -0400

    cambios recuperados

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -15,6 +15,7 @@
         <li><a href="/about">About Us</a></li>
         <li><a href="/services">Services</a></li>
         <li><a href="/posts">Posts</a></li>
+        <li><a href="/posts/create">Create Post</a></li>
       </ul>
     </div><!--/.nav-collapse -->
   </div>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,7 +4,7 @@
 	<a href="/posts" class="btn btn-default">Go Back</a>
 	<h1>{{ $post->title }}</h1>
 	<div>
-		{{ $post->body }}
+		{!! $post->body !!}
 	</div>
 	<br>
 	<small>Written on {{ $post->created_at }}</small>
